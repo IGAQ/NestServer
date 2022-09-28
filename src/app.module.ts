66e33ac38fdad6cc46a996a1452863b9b97dfdd0
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-09-28 09:56:33 -0700

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
 })
 export class AppModule {}
