20f7cabcc906b1fa2e2658200a5708ff1ec4e022
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-09-28 10:27:53 -0700

    format

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+    controllers: [AuthController],
+    providers: [AuthService],
 })
 export class AuthModule {}
