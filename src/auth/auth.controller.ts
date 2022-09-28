fe389f8ee31ab590a467b812607dd69bd07b708f
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-09-28 11:24:54 -0700

    auth service base code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,11 +7,11 @@ export class AuthController {
 
     @Post("signup")
     signup() {
-        return "I am signed up";
+        return this.authService.signup();
     }
 
     @Post("signin")
     signin() {
-        return "I am signed in";
+        return this.authService.signin();
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+    signup() {
+        return { msg: "I am signed up" };
+    }
+
+    signin() {
+        return { msg: "I am signed in" };
+    }
+}
