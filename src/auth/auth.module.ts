b8d56c4ce449c77471bbbe740efecfadaae37385
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-09-28 19:43:24 -0700

    import UserModule inside AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./controllers/auth.controller";
 import { AuthService } from "./services/auth.service";
+import { UsersModule } from "../users/users.module";
 
 @Module({
+    imports: [UsersModule],
     controllers: [AuthController],
     providers: [
         {
