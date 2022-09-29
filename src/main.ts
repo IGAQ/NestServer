847e10cfd66952506c52ceb3aecab919638e6c6a
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-09-28 20:10:00 -0700

    add cors
    Signed-off-by: Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import swaggerSetup from "./swaggerSetup";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: "*",
+        methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+        optionsSuccessStatus: 200,
+    });
+
     await swaggerSetup(app);
 
     await app.listen(3000);
