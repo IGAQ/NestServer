05da1186d85d7aeae20055fe48cf520f21eaa7f3
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-09-28 21:55:55 -0700

    Update auth.controller.ts

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
-import { AuthDto } from "../dto/auth.dto";
+import { AuthDto } from "../dto";
 import { IAuthService } from "../services/auth.service.interface";
 
 @ApiTags("auth")
@@ -9,13 +9,13 @@ export class AuthController {
     constructor(@Inject("IAuthService") private _authService: IAuthService) { }
 
     @Post("signup")
-    signup(
+    public signup(
         @Body() dto: AuthDto) {
         return this._authService.signup(dto);
     }
 
     @Post("signin")
-    signin() {
+    public signin() {
         return this._authService.signin();
     }
 }
