a8c021572be59bd8e33782fb3557b03b43d39f0c
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-10-01 13:09:09 -0700

    wrote a few more tests

--- src/auth/controllers/auth.controller.spec.ts
+++ src/auth/controllers/auth.controller.spec.ts
@@ -2,7 +2,6 @@ import { Test } from "@nestjs/testing";
 import { AuthServiceTest } from "../services/auth.service.test";
 import { AuthController } from "./auth.controller";
 import { UsersService } from "../../users/services/usersService/users.service";
-import { AuthDto } from "../models";
 
 describe("AuthController", () => {
     let authController: AuthController;
@@ -32,31 +31,41 @@ describe("AuthController", () => {
 
     describe("signin", () => {
         it("if signin is successful, should return a success message", async () => {
-            const dto = new AuthDto();
-            dto.email = "chris@gmail.com";
-            dto.password = "secret";
-            dto.username = "chris";
             expect(
                 await authController.signin({
-                    email: dto.email,
-                    password: dto.password,
-                    username: dto.username,
+                    email: "chris@gmail.com",
+                    password: "secret",
+                    username: "chris",
                 })
             ).toEqual({ msg: "I am signed in" });
+        });
+        it("if signin user does not exist, should return an error message", async () => {
+            expect(
+                await authController.signin({
+                    email: "bob@gmail.com",
+                    password: "secret",
+                    username: "bob",
+                })
+            ).toEqual({ msg: "User not found" });
+        });
+        it("if signin password is incorrect, should return an error message", async () => {
+            expect(
+                await authController.signin({
+                    email: "chris@gmail.com",
+                    password: "wrongpassword",
+                    username: "chris",
+                })
+            ).toEqual({ msg: "Incorrect password" });
         });
     });
 
     describe("signup", () => {
         it("if signup is successful, should return a success message", async () => {
-            const dto = new AuthDto();
-            dto.email = "ian@gmail.com";
-            dto.password = "ian123";
-            dto.username = "ian";
             expect(
                 await authController.signup({
-                    email: dto.email,
-                    password: dto.password,
-                    username: dto.username,
+                    email: "ian@gmail.com",
+                    password: "ian123",
+                    username: "ian",
                 })
             ).toEqual({ msg: "I am signed up" });
         });
