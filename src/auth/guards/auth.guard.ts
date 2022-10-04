4bbe3ef1fbbd0aee24580455d55fb6b0c97b0c0b
Ilia Abedian <eilia.abedian@yahoo.com>
2022-10-04 12:06:44 -0700

    fix the name of the class

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    public canActivate(
+        context: ExecutionContext
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            const request = context.switchToHttp().getRequest();
+            resolve(true);
+            return;
+        });
+    }
+}
