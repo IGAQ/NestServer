4e56f650386f0fce082d85d28948ca32fca496a2
Ilia Abedian <eilia.abedian@yahoo.com>
2022-10-04 13:59:11 -0700

    add authenticate route

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Post, Res, UseGuards } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { AuthDto } from "../models";
 import { IAuthService } from "../services/auth.service.interface";
+import { AuthGuard } from "@nestjs/passport";
+import { Response } from "express";
 
 @ApiTags("auth")
 @Controller("auth")
@@ -16,5 +18,17 @@ export class AuthController {
     @Post("signin")
     public signin(@Body() dto: AuthDto) {
         return this._authService.signin(dto);
+    }
+
+    @Post("authenticate")
+    // @UseGuards(AuthGuard("jwt"))
+    /**
+     * This route is for the front-end to use to check whether the user is authenticated or not.
+     * In other words, checks if the JWT token is still valid or not.
+     * If the AuthGuard PASSES the request, that means the user is authenticated. Therefore, the controller will send a
+     * 200 status.
+     */
+    public authenticate(@Res() response: Response) {
+        return response.sendStatus(200);
     }
 }
