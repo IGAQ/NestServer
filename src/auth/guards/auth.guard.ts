5a3c8a95d3f3c156db2005551d13b953bf0600e1
Ilia Abedian <eilia.abedian@yahoo.com>
2022-10-04 12:26:20 -0700

    add the necessary logic
    * add comments

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,15 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
 
+/**
+ * Use this guard when the user has to be logged-in.
+ * This guard will reject the request if the user is not authenticated (logged-in).
+ */
 @Injectable()
 export class AuthGuard implements CanActivate {
     public canActivate(
         context: ExecutionContext
     ): boolean | Promise<boolean> | Observable<boolean> {
-        return new Promise<boolean>((resolve, reject) => {
+        return new Promise<boolean>(resolve => {
             const request = context.switchToHttp().getRequest();
-            resolve(true);
-            return;
+
+            // If the `request.user` exists, pass; Otherwise, reject.
+            if (request.user !== undefined) resolve(true);
+            else resolve(false);
         });
     }
 }
