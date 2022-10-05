fbc2cb49100502f3d42c494215cda82b99248f96
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-04 17:53:50 -0700

    Update users.controller.spec.ts

--- src/users/controllers/users.controller.spec.ts
+++ src/users/controllers/users.controller.spec.ts
@@ -35,64 +35,90 @@ describe("UsersController", () => {
         expect(usersController).toBeDefined();
     });
 
-    describe("they are all giving successful promises", () => {
-        it("should return an array of users", async () => {
-            const result = await request(app.getHttpServer())
-                .get(`/users/`)
-                .set("Accept", "application/json");
+    describe("GET /users", () => {
+        describe("Happy Path", () => {
+            it("should return an array of users", async () => {
+                const result = await request(app.getHttpServer())
+                    .get(`/users/`)
+                    .set("Accept", "application/json");
 
-            expect(Array.isArray(result.body)).toBe(true);
-        });
-
-        it("should return a user by id", async () => {
-            const result = await request(app.getHttpServer())
-                .get(`/users/1`)
-                .set("Accept", "application/json");
-
-            expect(result.body).toMatchObject({
-                userId: 1,
-                username: "john",
-                email: "john@gmail.com",
+                expect(Array.isArray(result.body)).toBe(true);
             });
-
-            expect(result.body.password).toBeUndefined();
         });
+        describe("Sad Path", () => {
 
-        it("should return a user by username", async () => {
-            const result = await request(app.getHttpServer())
-                .get(`/users/username/john`)
-                .set("Accept", "application/json");
-
-            expect(result.body).toMatchObject({
-                userId: 1,
-                username: "john",
-                email: "john@gmail.com",
-            });
-
-            expect(result.body.password).toBeUndefined();
         });
     });
 
-    describe("they are all giving rejected promises", () => {
-        it("should return an error message if the user does not exist userById", async () => {
-            const result = await request(app.getHttpServer())
-                .get(`/users/666`)
-                .set("Accept", "application/json");
+    describe("GET /users/:id", () => {
+        describe("Happy Path", () => {
+            it("should return a user by id", async () => {
+                const result = await request(app.getHttpServer())
+                    .get(`/users/1`)
+                    .set("Accept", "application/json");
 
-            expect(result.body).toMatchObject({
-                message: "User not found",
-                statusCode: 404,
+                expect(result.body).toMatchObject({
+                    userId: 1,
+                    username: "john",
+                    email: "john@gmail.com",
+                });
+
+                expect(result.body.password).toBeUndefined();
             });
         });
+        describe("Sad Path", () => {
+            it("should return 403 if the user is not logged-in.", async () => {
+                const result = await request(app.getHttpServer())
+                    .get(`/users/666`)
+                    .set("Accept", "application/json");
 
-        it("should return an error message if the user does not exist userByUsername", async () => {
-            const result = await request(app.getHttpServer())
-                .get(`/users/username/666`)
-                .set("Accept", "application/json");
+                expect(result.status).toBe(403);
+                expect(result.body).toMatchObject({
+                    statusCode: 403,
+                    message: "Forbidden resource",
+                    error: "Forbidden"
+                });
+            });
 
-            expect(result.body).toMatchObject({
-                message: "User not found",
-                statusCode: 404,
+            it("should return an error message if the user does not exist userById", async () => {
+                const result = await request(app.getHttpServer())
+                    .get(`/users/666`)
+                    .set("Accept", "application/json");
+
+                expect(result.body).toMatchObject({
+                    message: "User not found",
+                    statusCode: 404,
+                });
+            });
+        });
+    });
+
+    describe("GET /users/:username", () => {
+        describe("Happy Path", () => {
+            it("should return a user by username", async () => {
+                const result = await request(app.getHttpServer())
+                    .get(`/users/username/john`)
+                    .set("Accept", "application/json");
+
+                expect(result.body).toMatchObject({
+                    userId: 1,
+                    username: "john",
+                    email: "john@gmail.com",
+                });
+
+                expect(result.body.password).toBeUndefined();
+            });
+        });
+        describe("Sad Path", () => {
+            it("should return an error message if the user does not exist userByUsername", async () => {
+                const result = await request(app.getHttpServer())
+                    .get(`/users/username/666`)
+                    .set("Accept", "application/json");
+
+                expect(result.body).toMatchObject({
+                    message: "User not found",
+                    statusCode: 404,
+                });
             });
         });
     });
