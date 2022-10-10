8085c21e1423cc6be02308a5fcea72996668f5fe
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-10 09:59:47 -0700

    make initial structure of the final service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,38 +1,27 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { UserDto, RegisterUserPayloadDto } from "../models";
 import { IUsersService } from "./users.service.interface";
-import { IPrismaService } from "../../prisma/prisma.service.interface";
 import { Neo4jService } from "../../neo4j/neo4j.service";
 
 @Injectable()
-export class UsersService implements IUsersService {
-    constructor(
-        @Inject("IPrismaService") private _prismaService: IPrismaService,
-        @Inject(Neo4jService) private _neo4jService: Neo4jService
-    ) {}
+export class UsersServicePrisma implements IUsersService {
+	constructor(
+		@Inject(Neo4jService) private _neo4jService: Neo4jService
+	) {}
 
-    public async findAll(): Promise<any> {
-        return await this._prismaService.user.findMany();
-    }
+	public async findAll(): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
 
-    public async findUserByUsername(username: string): Promise<UserDto | undefined> {
-        const foundUser = await this._prismaService.user.findFirst({ where: { username } });
+	public async findUserByUsername(username: string): Promise<UserDto | undefined> {
+		throw new Error("Method not implemented.");
+	}
 
-        return foundUser as any;
-    }
+	public async findUserById(userId: number): Promise<UserDto | undefined> {
+		throw new Error("Method not implemented.");
+	}
 
-    public async findUserById(userId: number): Promise<UserDto | undefined> {
-        const foundUser = await this._prismaService.user.findUnique({ where: { userId } });
-
-        return foundUser as any;
-    }
-
-    public async addUser(user: RegisterUserPayloadDto): Promise<void> {
-        const newUser = new UserDto();
-        newUser.username = user.username;
-        newUser.password = user.password;
-        newUser.email = user.email;
-
-        this._prismaService.user.create({ data: newUser as any });
-    }
+	public async addUser(user: RegisterUserPayloadDto): Promise<void> {
+		throw new Error("Method not implemented.");
+	}
 }
