5152e21843f9c7ac89e220e2f4e1fa57c2e0638b
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-10 19:48:50 -0700

    add initial CRU  for User entity

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,27 +1,105 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { UserDto, RegisterUserPayloadDto } from "../models";
+import { RegisterUserPayloadDto, Role, User } from "../models";
 import { IUsersService } from "./users.service.interface";
 import { Neo4jService } from "../../neo4j/neo4j.service";
+import { v4 as uuidv4 } from "uuid";
 
 @Injectable()
-export class UsersServicePrisma implements IUsersService {
+export class UsersService implements IUsersService {
 	constructor(
-		@Inject(Neo4jService) private _neo4jService: Neo4jService
-	) {}
-
-	public async findAll(): Promise<any> {
-		throw new Error("Method not implemented.");
+		@Inject(Neo4jService) private _neo4jService: Neo4jService,
+	) {
 	}
 
-	public async findUserByUsername(username: string): Promise<UserDto | undefined> {
-		throw new Error("Method not implemented.");
+	public async findAll(): Promise<User[]> {
+		const allUsers = await this._neo4jService.read(`MATCH (u:User) RETURN u`, {});
+		console.debug(allUsers);
+		return allUsers.records.map((record) => {
+			let user = record.get("u").properties;
+			user.role = user.role.map(r => r.low) as Role[];
+			return user;
+		});
 	}
 
-	public async findUserById(userId: number): Promise<UserDto | undefined> {
-		throw new Error("Method not implemented.");
+	public async findUserByUsername(username: string): Promise<User | undefined> {
+		const user = await this._neo4jService.read(`MATCH (u:User {username: $username}) RETURN u`, { username: username });
+		if (user.records.length === 0) return undefined;
+		let foundUser = user.records[0].get("u").properties;
+		foundUser.role = foundUser.role.map(r => r.low) as Role[];
+		return foundUser;
+	}
+
+	public async findUserById(userId: string): Promise<User | undefined> {
+		const user = await this._neo4jService.read(`MATCH (u:User {userId: $userId}) RETURN u`, { userId: userId });
+		if (user.records.length === 0) return undefined;
+		let foundUser = user.records[0].get("u").properties;
+		foundUser.role = foundUser.role.map(r => r.low) as Role[];
+		return foundUser;
 	}
 
 	public async addUser(user: RegisterUserPayloadDto): Promise<void> {
-		throw new Error("Method not implemented.");
+		this._neo4jService.write(`CREATE (u:User {
+			userId: $userId,
+			createdAt: $createdAt,
+			updatedAt: $updatedAt,
+			email: $email,
+			emailVerified: $emailVerified,
+			phoneNumber: $phoneNumber,
+			phoneNumberVerified: $phoneNumberVerified,
+			username: $username,
+			normalizedUsername: $normalizedUsername,
+			passwordHash: $passwordHash,
+			level: $level
+		})`,
+			{
+				userId: uuidv4(),
+
+				createdAt: new Date().getTime(),
+				updatedAt: new Date().getTime(),
+
+				email: user.email,
+				emailVerified: false,
+
+				phoneNumber: "",
+				phoneNumberVerified: false,
+
+				username: user.username,
+				normalizedUsername: user.username.toUpperCase(),
+
+				passwordHash: user.password,
+
+				role: [Role.USER],
+
+				level: 0,
+			} as Omit<User, "posts">);
 	}
+
+	public async updateUser(user: User): Promise<void> {
+		this._neo4jService.write(`MATCH (u:User {userId: $userId}) 
+		SET 
+			u.phoneNumber = $phoneNumber,
+			u.phoneNumberVerified = $phoneNumberVerified,
+			u.username = $username,
+			u.normalizedUsername = $normalizedUsername,
+			u.email = $email,
+			u.emailVerified = $emailVerified,
+			u.passwordHash = $passwordHash,
+			u.level = $level,
+			u.role = $role,
+			u.updatedAt = $updatedAt
+		`,
+			{
+				phoneNumber: user.phoneNumber,
+				phoneNumberVerified: user.phoneNumberVerified,
+				username: user.username,
+				normalizedUsername: user.username.toUpperCase(),
+				email: user.email,
+				emailVerified: user.emailVerified,
+				passwordHash: user.passwordHash,
+				level: user.level,
+				role: user.role,
+				updatedAt: new Date().getTime(),
+			} as Omit<User, "posts" | "userId" | "createdAt">);
+	}
+
 }
