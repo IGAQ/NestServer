866e4fc8a571c8058b4d984811e5ca1fa0da1f1f
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-12 15:17:43 -0700

    remove neo4j draft decorators

--- src/neo4j/neo4j.helper.types.ts
+++ src/neo4j/neo4j.helper.types.ts
@@ -1,14 +1,6 @@
-import { RelationshipDirection } from "./decorators/relationship.decorator";
-
-export interface EntitiesRelationshipsValue<T extends string | number | symbol, PropsType> {
-    directions: RelationshipDirection[];
-    relType: T;
-    props: PropsType;
-}
-
-export type RelatedEntity<T extends string | number | symbol, EntityType> = {
+export type RelatedEntity<EntityType, T extends string | number | symbol> = {
     [key in T]: {
-        relationships: EntitiesRelationshipsValue<T, object>;
         entities: Array<EntityType>;
+        relType: T;
     };
 };
