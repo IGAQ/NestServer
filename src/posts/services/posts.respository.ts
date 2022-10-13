3f3df5aa03f49e97103f66226cf37ff42a64bc4b
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-12 19:32:31 -0700

    draft - not sure if the queries work fine or not

--- src/posts/services/posts.respository.ts
+++ src/posts/services/posts.respository.ts
@@ -1,11 +1,12 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { Neo4jService } from "src/neo4j/neo4j.service";
 import { v4 as uuidv4 } from "uuid";
-import { Post } from "../models";
+import { RelatedEntityRecordItem } from "../../neo4j/neo4j.helper.types";
+import { Neo4jService } from "src/neo4j/neo4j.service";
 import { PostToSelfRelTypes, RestrictedProps } from "../models/toSelf";
-import { HasPostTypeProps, PostToPostTypeRelTypes } from "../models/toPostType";
-import { HasPostTagProps, PostToPostTagRelTypes } from "../models/toTags";
+import { PostToPostTypeRelTypes } from "../models/toPostType";
+import { PostToPostTagRelTypes } from "../models/toTags";
 import { PostToAwardRelTypes } from "../models/toAward";
+import { PostTag, PostType, Post } from "../models";
 
 @Injectable()
 export class PostsRepository {
@@ -13,22 +14,34 @@ export class PostsRepository {
 
     public async findAll(): Promise<Post[]> {
         const allPosts = await this._neo4jService.read(
-            `MATCH (p:Post)-[restrictedProps:${PostToSelfRelTypes.RESTRICTED}]->(p),
-            (p)-[hasPostTypeRel:${PostToPostTypeRelTypes.HAS_POST_TYPE}]->(postType:PostType),
-            (p)-[hasPostTagRel:${PostToPostTagRelTypes.HAS_POST_TAG}]->(postTag:PostTag),
+            `MATCH (p:Post)-[restrictedProps:${PostToSelfRelTypes.RESTRICTED}]->(p)
+            (p)-[:${PostToPostTypeRelTypes.HAS_POST_TYPE}]->(postType:PostType)
+            (p)-[:${PostToPostTagRelTypes.HAS_POST_TAG}]->(postTag:PostTag)
             (p)-[hasAwardRel:${PostToAwardRelTypes.HAS_AWARD}]->(award:Award)
-            RETURN p, restrictedProps, hasPostTypeRel, hasPostTagRel, hasAwardRel`,
+            RETURN p, restrictedProps, postType, postTag, award, hasAwardRel`,
             {}
         );
         let records = allPosts.records;
         console.log(records, "records for findAll");
         if (records.length === 0) return [];
         return records.map((record) => {
+            let awards = record.get("award");
+            let hasAwardRel = record.get("hasAwardRel");
+
+            console.log(awards, "awards");
+            console.log(hasAwardRel, "hasAwardRel");
+
+            let decoratedAwards = awards.map((award, index) => {
+                return new RelatedEntityRecordItem(award, hasAwardRel[index]);
+            });
+
             return new Post({
                 ...record.get("p").properties,
-                postType: new HasPostTypeProps(record.get("hasPostTypeRel").properties),
-                postTags: new HasPostTagProps(record.get("hasPostTagRel").properties),
-                awards: record.get("hasAwardRel") ?? [],
+                postType: new PostType(record.get("postType").properties),
+                postTags: new PostTag(record.get("postTag").properties),
+                awards: {
+                    [PostToAwardRelTypes.HAS_AWARD]: decoratedAwards,
+                },
                 restrictedProps: record.get("restrictedProps")?.properties ?? null,
             });
         });
@@ -51,14 +64,18 @@ export class PostsRepository {
 
     public async addPost(post: Post): Promise<void> {
         this._neo4jService.write(
-            `CREATE (p:Post {
+            `
+            MATCH (postType:PostType) WHERE postType.typeId = $postTypeId
+            CREATE (p:Post {
 			postId: $postId,
 			updatedAt: $updatedAt,
 			postContent: $postContent,
             postTitle: $postTitle,
             pending: $pending,
-		})`,
+		})-[:${PostToPostTypeRelTypes.HAS_POST_TYPE}]->(postType)
+		`,
             {
+                // Post
                 postId: uuidv4(),
 
                 updatedAt: new Date().getTime(),
@@ -67,6 +84,10 @@ export class PostsRepository {
                 postTitle: post.postTitle,
 
                 pending: false,
+
+                // Post.postType
+                postTypeId: post.postType.postTypeId,
+
             } as Omit<any, "posts">
         );
     }
