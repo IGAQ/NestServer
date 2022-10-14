d1fbea4ac50dbeedc570dbcb68311691360ea202
Ilia Abedian <eilia.abedian@yahoo.com>
2022-10-14 14:19:08 -0700

    fix nullable property

--- src/posts/models/post.ts
+++ src/posts/models/post.ts
@@ -35,7 +35,7 @@ export class Post {
     pending: boolean;
 
     @ApiProperty({ type: RestrictedProps })
-    restrictedProps?: RestrictedProps = null;
+    restrictedProps: Nullable<RestrictedProps> = null;
 
     constructor(partial?: Partial<Post>) {
         Object.assign(this, partial);
