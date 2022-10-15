bf9dee26af53cbf4a4b4308a0ee780c47f5de78e
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-15 10:30:05 -0700

    add comment to self relationship model

---
+++ src/comments/models/toSelf/replied.props.ts
@@ -0,0 +1,7 @@
+import { RelationshipProps } from "../../../neo4j/neo4j.helper.types";
+
+export class RepliedProps implements RelationshipProps {
+	constructor(partial?: Partial<RepliedProps>) {
+		Object.assign(this, partial);
+	}
+}
