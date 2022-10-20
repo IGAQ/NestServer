397167fc655f370fa388a98093d76c55f4c88b4f
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-10-20 12:31:50 -0700

    add generateId method in Neo4jService

--- src/neo4j/services/neo4j.service.ts
+++ src/neo4j/services/neo4j.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, Logger } from "@nestjs/common";
 import { NEO4J_DRIVER, NEO4J_OPTIONS } from "../neo4j.constants";
 import { Driver, Result, session, Session } from "neo4j-driver";
 import { Neo4jConfig } from "../neo4jConfig.interface";
+import { v4 as uuidv4 } from "uuid";
 
 @Injectable()
 export class Neo4jService {
@@ -12,6 +13,10 @@ export class Neo4jService {
     constructor(@Inject(NEO4J_OPTIONS) config: Neo4jConfig, @Inject(NEO4J_DRIVER) driver: Driver) {
         this._driver = driver;
         this._config = config;
+    }
+
+    public generateId(): string {
+        return uuidv4();
     }
 
     public getReadSession(database?: string): Session {
