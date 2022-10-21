ebec38ba7754e1db1877341269cb1e18f2065f1d
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-10-21 11:41:15 -0700

    completed basic postTag repository service

--- src/posts/services/postTagRepository/postTags.repository.interface.ts
+++ src/posts/services/postTagRepository/postTags.repository.interface.ts
@@ -1,5 +1,16 @@
 import { PostTag } from "../../models";
 
 export interface IPostTagsRepository {
+    findAll(): Promise<PostTag[]>;
+
     getPostTagsByPostId(postId: string): Promise<PostTag[]>;
+
+    getPostTagByTagId(tagId: string): Promise<PostTag | undefined>;
+
+    addPostTag(postTag: PostTag): Promise<PostTag>;
+
+    updatePostTag(postTag: PostTag): Promise<void>;
+
+    deletePostTag(postTagId: string): Promise<void>;
 }
+
