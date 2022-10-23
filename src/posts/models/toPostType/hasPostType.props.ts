4f3f166e32d0df56887dc875fe15cafff1a6c65a
Ian Chao <ianxchao@gmail.com>
2022-10-22 20:08:19 -0700

    Revert "removing props"
    This reverts commit 857b6588f261afa67ed95932e0a4ae9c35d141b5.

--- src/posts/models/toPostType/hasPostType.props.ts
+++ src/posts/models/toPostType/hasPostType.props.ts
@@ -1,6 +1,14 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { RelationshipProps } from "../../../neo4j/neo4j.helper.types";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class HasPostTypeProps implements RelationshipProps {
+    @ApiProperty({ type: String, format: "uuid" })
+    postTypeId: string;
 
+    @ApiProperty({ type: String })
+    postType: string;
+
+    constructor(partial?: Partial<HasPostTypeProps>) {
+        Object.assign(this, partial);
+    }
 }
