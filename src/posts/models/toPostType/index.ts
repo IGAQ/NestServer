35022268ab7b0988e84dd1badd5bb7def476291d
Ian Chao <ianxchao@gmail.com>
2022-10-22 18:27:33 -0700

    removing relationship props for posttype and posttag

--- src/posts/models/toPostType/index.ts
+++
@@ -1,5 +0,0 @@
-export { HasPostTypeProps } from "./hasPostType.props";
-
-export enum PostToPostTypeRelTypes {
-    HAS_POST_TYPE = "HAS_POST_TYPE",
-}
--- src/posts/models/toTags/index.ts
+++
@@ -1,5 +0,0 @@
-export { HasPostTagProps } from "./hasPostTag.props";
-
-export enum PostToPostTagRelTypes {
-    HAS_POST_TAG = "HAS_POST_TAG",
-}
