857b6588f261afa67ed95932e0a4ae9c35d141b5
Ian Chao <ianxchao@gmail.com>
2022-10-22 19:16:21 -0700

    removing props

--- src/posts/models/toPostType/hasPostType.props.ts
+++ src/posts/models/toPostType/hasPostType.props.ts
@@ -1,14 +1,6 @@
-import { RelationshipProps } from "../../../neo4j/neo4j.helper.types";
 import { ApiProperty } from "@nestjs/swagger";
+import { RelationshipProps } from "../../../neo4j/neo4j.helper.types";
 
 export class HasPostTypeProps implements RelationshipProps {
-    @ApiProperty({ type: String, format: "uuid" })
-    postTypeId: string;
 
-    @ApiProperty({ type: String })
-    postType: string;
-
-    constructor(partial?: Partial<HasPostTypeProps>) {
-        Object.assign(this, partial);
-    }
 }
