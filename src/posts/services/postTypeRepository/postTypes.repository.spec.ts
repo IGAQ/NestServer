9054ebbc6e85a363004b72deea675a808ab5d783
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-10-24 10:24:25 -0700

    postTypes repository and tests

---
+++ src/posts/services/postTypeRepository/postTypes.repository.spec.ts
@@ -0,0 +1,105 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { IPostTypesRepository } from "./postTypes.repository.interface";
+import { PostTypesRepository } from "./postTypes.repository";
+import { PostType } from "../../models";
+import { Neo4jService } from "src/neo4j/services/neo4j.service";
+import { Neo4jConfig } from "src/neo4j/neo4jConfig.interface";
+import { NEO4J_DRIVER, NEO4J_OPTIONS } from "../../../neo4j/neo4j.constants";
+import { createDriver } from "../../../neo4j/neo4j.utils";
+import { neo4jCredentials } from "../../../_domain/constants";
+import { Neo4jSeedService } from "../../../neo4j/services/neo4j.seed.service";
+import { RestrictedProps } from "../../../_domain/models/toSelf";
+import { _$ } from "../../../_domain/injectableTokens";
+
+describe("PostTypesRepository", () => {
+    let postTypesRepository: IPostTypesRepository;
+    let neo4jSeedService: Neo4jSeedService;
+    let seedCalled = false;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                {
+                    provide: NEO4J_OPTIONS,
+                    useFactory: (): Neo4jConfig => neo4jCredentials,
+                },
+                {
+                    provide: NEO4J_DRIVER,
+                    inject: [NEO4J_OPTIONS],
+                    useFactory: async () => createDriver(neo4jCredentials),
+                },
+                Neo4jService,
+                Neo4jSeedService,
+                {
+                    provide: _$.IPostTypesRepository,
+                    useClass: PostTypesRepository,
+                },
+            ],
+        }).compile();
+
+        postTypesRepository = module.get<PostTypesRepository>(_$.IPostTypesRepository);
+
+        neo4jSeedService = module.get<Neo4jSeedService>(Neo4jSeedService);
+        try {
+            if (!seedCalled) {
+                seedCalled = true;
+                await neo4jSeedService.seed();
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    it("should be defined", () => {
+        expect(postTypesRepository).toBeDefined();
+        expect(neo4jSeedService).toBeDefined();
+    });
+
+    describe(".findAll()", () => {
+        let postTypes: PostType[];
+
+        beforeAll(async () => {
+            postTypes = await postTypesRepository.findAll();
+        });
+
+        it("should return an array of PostType objects", () => {
+            expect(postTypes).toBeInstanceOf(Array);
+            expect(postTypes[0]).toBeInstanceOf(PostType);
+        });
+
+        it("should return an array of PostType objects with the correct properties", () => {
+            const postType = postTypes[0];
+            const restrictedProps = new RestrictedProps();
+            const postTypeProps = Object.keys(postType);
+            const restrictedPropsKeys = Object.keys(restrictedProps);
+
+            expect(postTypeProps).toEqual(expect.arrayContaining(restrictedPropsKeys));
+        });
+    });
+
+    describe(".findPostTypeById()", () => {
+        let postType: PostType | undefined;
+
+        beforeAll(async () => {
+            postType = await postTypesRepository.findPostTypeById("1");
+        });
+
+        it("should return the story post type", () => {
+            expect(postType).toBeInstanceOf(PostType);
+            expect(postType?.postType).toBe("Story");
+        });
+
+        it("should return a PostType object", () => {
+            expect(postType).toBeInstanceOf(PostType);
+        });
+
+        it("should return a PostType object with the correct properties", () => {
+            const restrictedProps = new RestrictedProps();
+            const postTypeProps = Object.keys(postType);
+            const restrictedPropsKeys = Object.keys(restrictedProps);
+
+            expect(postTypeProps).toEqual(expect.arrayContaining(restrictedPropsKeys));
+        });
+    });
+});
+
--- src/posts/services/postTypeRepository/postTypes.repository.ts
+++ src/posts/services/postTypeRepository/postTypes.repository.ts
@@ -1,12 +1,27 @@
 import { IPostTypesRepository } from "./postTypes.repository.interface";
 import { PostType } from "../../models";
+import { Inject, Injectable } from "@nestjs/common";
+import { Neo4jService } from "src/neo4j/services/neo4j.service";
+import { _$ } from "src/_domain/injectableTokens";
 
+@Injectable()
 export class PostTypesRepository implements IPostTypesRepository {
+    constructor(@Inject(Neo4jService) private _neo4jService: Neo4jService) {}
+
     public async findAll(): Promise<PostType[]> {
-        throw new Error("Method not implemented.");
+        const allPostTypes = await this._neo4jService.read(`MATCH (t:PostType) RETURN t`, {});
+        let records = allPostTypes.records;
+        if (records.length === 0) return [];
+        return records.map(record => new PostType(record.get("t").properties));
     }
 
     public async findPostTypeById(postTypeId: string): Promise<PostType | undefined> {
-        throw new Error("Method not implemented.");
+        const postType = await this._neo4jService.read(
+            `MATCH (t:PostType) WHERE t.postTypeId = $postTypeId RETURN t`,
+            { postTypeId: postTypeId }
+        );
+        if (postType.records.length === 0) return undefined;
+        return new PostType(postType.records[0].get("t").properties);
     }
 }
+
