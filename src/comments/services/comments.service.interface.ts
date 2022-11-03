d06d5a002a7ed4b610c5fe3bd79212355f575f8f
Ian Chao <ianxchao@gmail.com>
2022-11-03 15:46:24 -0700

    Update comments.service.interface.ts

--- src/comments/services/comments.service.interface.ts
+++ src/comments/services/comments.service.interface.ts
@@ -3,4 +3,10 @@ import { CommentCreationPayloadDto } from "../models/commentCreationPayload.dto"
 
 export interface ICommentsService {
     authorNewComment(commentPayload: CommentCreationPayloadDto): Promise<Comment>;
+
+    findCommentById(commentId: string): Promise<Comment>;
+
+    markAsPinned(commentId: string): Promise<void>;
+
+    markAsDeleted(commentId: string): Promise<void>;
 }
