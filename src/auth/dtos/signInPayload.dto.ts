4d26a1c31bc41585683ccc3d1335c7d043abde37
Ilia A. Amiri <37903573+iliaamiri@users.noreply.github.com>
2022-11-14 11:23:19 -0800

    add documentations

--- src/auth/dtos/signInPayload.dto.ts
+++ src/auth/dtos/signInPayload.dto.ts
@@ -1,12 +1,25 @@
 import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class SignInPayloadDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        type: String,
+        minLength: 3,
+        maxLength: 12,
+        description: "The username of the user",
+    })
     username: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({
+        type: String,
+        minLength: 4,
+        maxLength: 20,
+        description: "The password of the user",
+    })
     password: string;
 
     constructor(partial?: Partial<SignInPayloadDto>) {
