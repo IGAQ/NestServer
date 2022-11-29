29995bb1889dfa9f6cfda35974309d9f515166a4
Ilia A. Amiri <37903573+iliaamiri@users.noreply.github.com>
2022-11-28 17:49:15 -0800

    fix a few bugs

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { Reflector } from "@nestjs/core";
 import { ROLES_KEY } from "../decorators/roles.decorator";
-import { Role } from "../../users/models";
+import { Role, User } from "../../users/models";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -18,7 +18,9 @@ export class RolesGuard implements CanActivate {
             return true;
         }
 
-        const { user } = context.switchToHttp().getRequest();
+        const request = context.switchToHttp().getRequest();
+        const user: User = request.user;
+        console.log(user);
         if (!user) {
             return false;
         }
