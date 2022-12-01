58e3e02bf796701711f79d43e550a1adfcfcf3ff
Ilia A. Amiri <37903573+iliaamiri@users.noreply.github.com>
2022-12-01 03:24:39 -0800

    add events' dtos for posts

--- src/posts/events/postGotVote.event.ts
+++ src/posts/events/postGotVote.event.ts
@@ -1,5 +1,13 @@
-export class PostGotUpvoteEvent {
+export class PostGotVoteEvent {
+    subscriberId: UUID;
+
     postId: UUID;
+
     username: string;
+
     avatar: string;
+
+    constructor(partial?: Partial<PostGotVoteEvent>) {
+        Object.assign(this, partial);
+    }
 }
